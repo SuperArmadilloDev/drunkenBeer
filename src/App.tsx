b6d775dba7a10d92a324ad2f9ff236acf00c3e88
SuperArmadilloDev <follietmartin@gmail.com>
2023-04-12 15:21:05 +0200

    fetch api for beer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
+import { Beer } from './common/types';
 
 function App() {
+  const [beers, setBeers] = useState<Beer[]>([]);
+  const fetchBeerData = () => {
+    fetch('https://api.punkapi.com/v2/beers')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setBeers(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchBeerData();
+  }, []);
+
   return (
     <div className='App'>
-      <h1>Hey!</h1>
+      <h1>Beer Names to test</h1>
+      <ul>
+        {beers.map((beers) => (
+          <li>{beers.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/common/types.ts
@@ -0,0 +1,70 @@
+// Main type
+export type Beer = {
+  id: string;
+  name: string;
+  tagline?: string;
+  first_brewed?: string;
+  description?: string;
+  image_url?: string;
+  abv?: number;
+  ibu?: number;
+  target_fg?: number;
+  target_og?: number;
+  ebc?: number;
+  srm?: number;
+  ph?: number;
+  attenuation_level?: number;
+  volume?: Volume;
+  boil_volume?: BoilVolume;
+  method?: Method;
+  ingredients?: Ingredients;
+  food_pairing?: string[];
+  brewers_tips: string;
+  contributed_by: string;
+};
+
+// Secondary types
+export type Volume = {
+  value?: number;
+  unit?: string;
+};
+
+export type BoilVolume = {
+  value?: number;
+  unit?: string;
+};
+
+export type Method = {
+  mash_temp?: MashTemp[];
+  fermentation?: {
+    temp?: Temp;
+  };
+  twist?: string;
+};
+
+export type Ingredients = {
+  malt?: Ingredient[];
+  hops?: Ingredient[];
+  yeast?: string;
+};
+
+// Third types
+export type MashTemp = {
+  temp?: Temp;
+  duration?: number;
+};
+
+export type Temp = {
+  value?: number;
+  unit?: string;
+};
+
+export type Ingredient = {
+  name?: string;
+  amount?: Amount;
+};
+
+export type Amount = {
+  value?: number;
+  unit?: string;
+};
